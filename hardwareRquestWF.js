7845b0d5b1a394bea86269bc7a0f6378e8b6ee6c
tbevilacqua <timothy.bevilacqua@outlook.com>
2020-02-21 15:44:34 -0500

    Work flow script needs sys-id for location AND stock room based on how old script was written.

--- hardwareRquestWF.js
+++ hardwareRquestWF.js
@@ -1,23 +1,28 @@
 task.short_description = 'Configure and Deliver new device - ' + current.cat_item.getDisplayValue();
-//  task.setValue('assignment_group', current.request.requested_for.location.u_default_support_group);
+//old //  task.setValue('assignment_group', current.request.requested_for.location.u_default_support_group);
 
-if (current.request.requested_for.location == "Atran"){  // sys id needed for location or will strings work?
-    task.setValue('assignment_group',"Stock - IT - Atran"); //sysid 258fd848dbcb841068b5797b8c9619ed
+//new//
+
+//Atran
+if (current.request.requested_for.location == "d6cf0b19db54d300c5215878dc9619e3"){
+    task.setValue('assignment_group',"258fd848dbcb841068b5797b8c9619ed"); // Stock - IT - Atran sysid
 
 }
 else {
-    if (current.request.requested_for.location == "1542a025db18d300c5215878dc9619b3"){   //sys_id of rlocation of Branford
-        task.setValue('assignment_group',"Stock - IT - Branford"); //sysid 94efd848dbcb841068b5797b8c9619fb
+//Branford
+    if (current.request.requested_for.location == "1542a025db18d300c5215878dc9619b3"){
+        task.setValue('assignment_group',"94efd848dbcb841068b5797b8c9619fb"); //    Stock - IT - Branford sysid
 
     }
     else {
-          if (current.request.requested_for.location == "Union Square"){
-              //task.setValue('assignment_group',"Stock - IT - Union Square"); //sysid ac10e808dbcb841068b5797b8c9619ce
-              task.setValue('assignment_group',"ac10e808dbcb841068b5797b8c9619ce");
+//Union Square
+          if (current.request.requested_for.location == "5483d233db4ac34024f77b5b8c96194d"){
+            task.setValue('assignment_group',"ac10e808dbcb841068b5797b8c9619ce"); //    Stock - IT - Union Square sysid
 
           }
           else {
-            task.setValue('assignment_group',"Stock - IT - Stamford"); //sysid a17ad048dbcb841068b5797b8c9619b9
+//Stamford & Remote
+            task.setValue('assignment_group',"a17ad048dbcb841068b5797b8c9619b9");  //    Stock - IT - Stamford sysid
 
     }
   }
